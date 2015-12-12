6d9b3e36c3248d7f82de2b871e1fca0cfd503345
znatz <z.nat.cn@gmail.com>
2015-12-12 13:57:34 +0900

    Initial Commit

---
+++ imageProcessing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  imageProcessing
+//
+//  Created by 趙自然 on 12/12/15.
+//  Copyright © 2015 趙自然. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ imageProcessing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  imageProcessing
+//
+//  Created by 趙自然 on 12/12/15.
+//  Copyright © 2015 趙自然. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
